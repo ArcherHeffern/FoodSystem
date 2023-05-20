c8169216f7092a24888147fb14447c39e6e07f38
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-05-19 17:22:32 -0700

    api call added for foodbanks

---
+++ backend/foodBankCall.ts
@@ -0,0 +1,12 @@
+import fetch = require('node-fetch');
+
+const getFoodBankApi= async(location: string) => {
+    try {
+        let url = `https://www.givefood.org.uk/api/2/foodbanks/?${location}/`;
+        const response = await fetch(url)
+        const responseJson = await response.json();
+        return responseJson.data;
+    } catch {(error) => {
+        console.log("something went wrong with the food bank api call", error);
+    }}
+}
